5f453bcf4465bd2eb5bb4f31b92cd235bcce96da
KiKiKi <chaika.design@gmail.com>
2019-01-30 01:56:47 +0900

    express static server

---
+++ express-server/README.md
@@ -0,0 +1,13 @@
+# express static server
+
+## set up
+
+```sh
+$ npm install
+```
+
+## run server
+
+```sh
+$ node server.js
+```
---
+++ express-server/server.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+      path    = require('path'),
+      app     = express();
+
+const rootPath = '/webroot';
+
+// テンプレートエンジン
+app.set('views', path.join(__dirname + rootPath) );
+app.set('view engine', 'pug');
+
+// 静的ファイル
+app.use(express.static(__dirname + rootPath, { index: false }));
+
+// Routing
+app.get('/', (req, res)=> {
+  res.render('index');
+});
+
+app.get('/:dir', (req, res)=> {
+  console.log(req.params.dir);
+  res.render(req.params.dir + '/index', {title: req.params.dir});
+});
+
+// Error
+app.use((err, req, res, next)=> {
+  console.log('>> ERROR >>', err);
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(3000, ()=> {
+  console.log("Express Server\nopen: http://localhost:3000");
+});
